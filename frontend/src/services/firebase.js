2e7ee31cce83082a2330bc8f43a6ed45e5a66fd2
Imran Shaikh <imranance99@gmail.com>
2025-03-25 23:27:48 +0530

    updated

--- frontend/src/services/firebase.js
+++ frontend/src/services/firebase.js
@@ -2,14 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBMOTvnqPQkkIQULVCT1Hsjr7b59cl4lZ4",  // Replace this with your actual API key
-  authDomain: "letter-editor-app-1589d.firebaseapp.com",
-  projectId: "letter-editor-app-1589d",
-  storageBucket: "letter-editor-app-1589d.appspot.com",
-  messagingSenderId: "524555696982",
-  appId: "1:524555696982:web:c05ab70b5c31db231034f7",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
-
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
